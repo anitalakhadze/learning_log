20742f8bad7a917bb0b4c0e72daeb65e495a0e14
atala15 <atala15@freeuni.edu.ge>
2020-07-11 14:49:51 +0400

    modified Topic model by adding a foreign key relationship to a user.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,7 +5,7 @@ class Topic(models.Model):
     """A Topic the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = ForeignKey(User)
+    owner = models.ForeignKey(User)
     
     def __str__(self):
         """Return a string representation of the model."""
